22e4facc748e1b1e1ec45224b43c44d31ac7f677
RinatSa <sadykov.r@seznam.cz>
2023-06-10 18:49:47 +0200

    Update: update Category component

--- components/category.js
+++ components/category.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Category(props) {
+
+    const [activeCategory, setActiveCategory] = useState()
+
+    const category = [
+        {id: 0, label: "All"},
+        {id: 1, label: "Meat"},
+        {id: 2, label: "Vegetarian"},
+        {id: 3, label: "Grill"},
+        {id: 4, label: "Hot"},
+        {id: 5, label: "Calzone"},
+    ]
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {category.map(item => <li key={item.id} onClick={() => setActiveCategory(item.id)}
+                                          className={activeCategory === item.id ? "active" : ""}>{item.label}</li>)}
             </ul>
         </div>
     );

892a26b3944c38d958b8ff20598c5c6ae001569e
RinatSa <sadykov.r@seznam.cz>
2023-06-13 11:56:28 +0200

    Add: add pizzaCart component Update: add new reducers in pizzaSlice

--- redux/pizzaSlice.js
+++ redux/pizzaSlice.js
@@ -11,13 +11,45 @@ export const pizzaSlice = createSlice({
     initialState,
     reducers: {
         addPizzas(state, action) {
-            state.pizzas.push(action.payload)
+            const actualPizza = state.pizzas.find(item => item.title === action.payload.title)
+            if (actualPizza) {
+                actualPizza.count++
+            } else {
+                state.pizzas.push(action.payload)
+            }
             state.totalCount = state.pizzas.reduce((a, b) => a + b.count, 0)
-            state.totalPrice = state.pizzas.reduce((a, b) => a + b.price, 0)
-
+            state.totalPrice = state.pizzas.reduce((a, b) => a + (b.price * b.count), 0)
+        },
+        clearAll(state) {
+            state.pizzas = []
+            state.totalCount = state.pizzas.reduce((a, b) => a + b.count, 0)
+            state.totalPrice = state.pizzas.reduce((a, b) => a + (b.price * b.count), 0)
+        },
+        plus(state, action) {
+            const actualPizza = state.pizzas.find(item => item.title === action.payload)
+            actualPizza.count++
+            state.totalCount = state.pizzas.reduce((a, b) => a + b.count, 0)
+            state.totalPrice = state.pizzas.reduce((a, b) => a + (b.price * b.count), 0)
+        },
+        minus(state, action) {
+            const actualPizza = state.pizzas.find(item => item.title === action.payload)
+            if (actualPizza.count === 1) {
+                state.pizzas = state.pizzas.filter(item => item.title !== action.payload)
+                state.totalCount = state.pizzas.reduce((a, b) => a + b.count, 0)
+                state.totalPrice = state.pizzas.reduce((a, b) => a + (b.price * b.count), 0)
+            } else {
+                actualPizza.count--
+                state.totalCount = state.pizzas.reduce((a, b) => a + b.count, 0)
+                state.totalPrice = state.pizzas.reduce((a, b) => a + (b.price * b.count), 0)
+            }
+        },
+        clearLine(state, action) {
+            state.pizzas = state.pizzas.filter(item => item.id !== action.payload)
+            state.totalCount = state.pizzas.reduce((a, b) => a + b.count, 0)
+            state.totalPrice = state.pizzas.reduce((a, b) => a + (b.price * b.count), 0)
         }
     }
 })
 
-export const {addPizzas} = pizzaSlice.actions
+export const {addPizzas, clearAll, plus, minus, clearLine} = pizzaSlice.actions
 export default pizzaSlice.reducer
